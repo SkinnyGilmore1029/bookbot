12529dbf4640b0ea0f4ff7edf6925d20f4f29b71
JGilmore1029 <Skinny_Gilmore@hotmail.com>
2025-03-06 09:53:51 -0600

    wrote get_book_text and main functions

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("testings to see if its synced")
+def get_book_text(filepath:str)->str:
+    with open(filepath) as f:
+        file_contents:str = f.read()
+    return file_contents
+
+def main(filepath:str)->str:
+    words = get_book_text(filepath)
+    return words
+
+if __name__ == "__main__":
+    main()

14f746469311e30f4e75afdaba81dd235a8b460f
JGilmore1029 <Skinny_Gilmore@hotmail.com>
2025-03-07 06:51:43 -0600

    Added count_words

--- main.py
+++ main.py
@@ -7,5 +7,12 @@ def main()->str:
     words = get_book_text("books/frankenstein.txt")
     print(words)
 
+def count_words()->str:
+    words = get_book_text("books/frankenstein.txt")
+    counted_words = str(len(words.split()))
+    print(count_words)
+    
+
 if __name__ == "__main__":
     main()
+    count_words()

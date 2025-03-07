0d71fd78d04a62b4b522bff87c38100ea2cc2c26
JGilmore1029 <Skinny_Gilmore@hotmail.com>
2025-03-07 07:07:30 -0600

    refactored code split up functions

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ def get_book_text(filepath:str)->str:
         file_contents:str = f.read()
     return file_contents
 
+def get_word_count(words)->str:
+    counted_words = words.split()
+    return len(counted_words)
+
 def main()->str:
     words = get_book_text("books/frankenstein.txt")
-    spilt_words = words.split()
-    print(str(len(spilt_words)) + " words found in the document")
+    count_words = get_word_count(words)
+    return len(count_words)
 
-
-    
 
 if __name__ == "__main__":
     main()

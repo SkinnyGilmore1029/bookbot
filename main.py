3404cd804a70577497d3f559af42433d37e0e8b6
JGilmore1029 <Skinny_Gilmore@hotmail.com>
2025-03-07 07:12:10 -0600

    Added stats.py for assignment

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
+from stats import get_word_count
+
 def get_book_text(filepath:str)->str:
     with open(filepath) as f:
         file_contents:str = f.read()
     return file_contents
 
-def get_word_count(words)->str:
-    counted_words = words.split()
-    return len(counted_words)
 
 def main()->str:
     words = get_book_text("books/frankenstein.txt")
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_word_count(words)->str:
+    counted_words = words.split()
+    return len(counted_words)

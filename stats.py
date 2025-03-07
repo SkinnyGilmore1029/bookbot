acd489c8236fcdaca56b5d9880e6f218fb55c9ec
JGilmore1029 <Skinny_Gilmore@hotmail.com>
2025-03-07 07:27:05 -0600

    counting characters 1.3

--- stats.py
+++ stats.py
@@ -8,8 +8,8 @@ def get_characters(words)->None:
     for word in words_list:
         word.lower()
         for w in word:
-            if character_dict[w] in character_dict.keys():
-                character_dict[w] +=1
-            elif character_dict[w] not in character_dict.keys():
-                character_dict[w] = 1
+            if w in character_dict.keys():
+                w +=1
+            elif w not in character_dict.keys():
+                w = 1
     return character_dict
